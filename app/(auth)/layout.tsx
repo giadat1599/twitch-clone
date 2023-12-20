2e3cde5dbeba8713dba33801a2311bb0a182aea0
Gia Dat Truong <giadattruong@192.168.2.5>
2023-12-20 22:59:01 +0700

    feat: auth setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from "react";
+import { Logo } from "./_components/logo";
+
+export default function AuthLayout({ children }: PropsWithChildren) {
+   return (
+      <div className="h-full flex flex-col items-center justify-center space-y-6">
+         <Logo />
+         {children}
+      </div>
+   );
+}
