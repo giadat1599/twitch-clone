7d58d1e669d7c43e643ab3d0ecd0e4119c287824
Gia Dat Truong <giadattruong@192.168.2.5>
2023-12-26 23:02:19 +0700

    feat: follow service

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
          <html lang="en">
             <body className={inter.className}>
                <ThemeProvider attribute="class" forcedTheme="dark" storageKey="gamehub-theme">
+                  <Toaster theme="light" position="bottom-center" />
                   {children}
                </ThemeProvider>
             </body>
