ce0a32840c626046a08bb7adab8f384870bf0ce0
Gia Dat Truong <giadattruong@192.168.2.5>
2024-01-04 16:47:00 +0700

    feat: live header

---
+++ components/stream-player/actions.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useTransition } from "react";
+import { toast } from "sonner";
+import { Heart } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { onFollow, onUnfollow } from "@/actions/follow";
+
+interface ActionsProps {
+   hostIdentity: string;
+   isFollowing: boolean;
+   isHost: boolean;
+}
+
+export const Actions = ({ hostIdentity, isFollowing, isHost }: ActionsProps) => {
+   const { userId } = useAuth();
+   const router = useRouter();
+   const [isPending, startTranisition] = useTransition();
+
+   const handleFollow = () => {
+      startTranisition(() => {
+         onFollow(hostIdentity)
+            .then((data) => toast.success(`You are now following ${data.following.username}`))
+            .catch(() => toast.error("Something went wrong"));
+      });
+   };
+
+   const handleUnfollow = () => {
+      startTranisition(() => {
+         onUnfollow(hostIdentity)
+            .then((data) => toast.success(`You have unfollowed ${data.following.username}`))
+            .catch(() => toast.error("Something went wrong"));
+      });
+   };
+
+   const toggleFollow = () => {
+      if (!userId) {
+         return router.push("/sign-in");
+      }
+
+      if (isHost) return;
+
+      if (isFollowing) {
+         handleUnfollow();
+      } else {
+         handleFollow();
+      }
+   };
+
+   return (
+      <Button
+         onClick={toggleFollow}
+         variant="primary"
+         size="sm"
+         className="w-full lg:w-auto"
+         disabled={isPending || isHost}
+      >
+         <Heart className={cn("h-4 w-4 mr-2", isFollowing ? "fill-white" : "fill-none")} />
+         {isFollowing ? "Unfollow" : "Follow"}
+      </Button>
+   );
+};
+
+export const ActionsSkeleton = () => {
+   return <Skeleton className="h-10 w-full lg:w-24" />;
+};
