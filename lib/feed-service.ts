f14bb3148b84c1593f3b1f83bb7f5b6f4bf195d6
Gia Dat Truong <giadattruong@192.168.2.5>
2024-01-06 16:22:12 +0700

    feat: home page

---
+++ lib/feed-service.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { db } from "./db";
+import { getSelf } from "./auth-service";
+
+export const getStream = async () => {
+   let userId;
+   try {
+      const self = await getSelf();
+      userId = self.id;
+   } catch {
+      userId = null;
+   }
+
+   let streams = [];
+
+   if (userId) {
+      streams = await db.stream.findMany({
+         where: { user: { NOT: { blocking: { some: { blockedId: userId } } } } },
+         select: { id: true, thumbnailUrl: true, name: true, isLive: true, user: true },
+         orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+      });
+   } else {
+      streams = await db.stream.findMany({
+         select: { id: true, thumbnailUrl: true, name: true, isLive: true, user: true },
+         orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+      });
+   }
+
+   return streams;
+};
